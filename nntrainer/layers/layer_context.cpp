e0fb1f8390477b97523ed7bcc76bb3cbaa94a3d4
Hyeonseok Lee <hs89.lee@samsung.com>
2026-07-27 18:47:54 +0900

    [graph/layers] Fix std::vector out-of-bounds crash in layers with empty inputs
    - Added safety check inputs.empty() in RunLayerContext::validate() to protect inputs[0] indexing
    - Added safety checks !inputs.empty() and inputs.size() > 1 in NetworkGraph::finalizeContext() and refinalizeContext() to protect inplace memory referencing
    
    Signed-off-by: Hyeonseok Lee <hs89.lee@samsung.com>

--- nntrainer/layers/layer_context.cpp
+++ nntrainer/layers/layer_context.cpp
@@ -564,7 +564,7 @@ bool RunLayerContext::validate(bool skip_input, bool skip_label) {
 #ifdef DEBUG
   std::function<bool(const Var_Grad *, bool)> matcher;
 
-  if (tensor_map.empty() || !tensor_map[inputs[0]->getName()]) {
+  if (tensor_map.empty() || (!inputs.empty() && !tensor_map[inputs[0]->getName()])) {
     auto filler = [this](const auto &vec) {
       for (auto const &val : vec) {
         if (val->getVariableRef().getTensorType().data_type ==
